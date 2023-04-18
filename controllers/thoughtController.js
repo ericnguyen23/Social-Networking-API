473c8831915389cddfd1db826cfca11819573a46
Eric <ericnguyen23@gmail.com>
2023-04-18 14:05:13 -0700

    Created get single thought get method

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -11,6 +11,16 @@ module.exports = {
       }
     });
   },
+  // Get single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   // create thought
   createThought(req, res) {
     Thought.create(req.body)
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const {
   getThoughts,
   createThought,
+  getSingleThought,
 } = require("../../controllers/thoughtController");
 
 // route = /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
+
+// route = /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought);
 
 module.exports = router;
