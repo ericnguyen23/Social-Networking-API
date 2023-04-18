2544baf88eef96e8ce754b1be9bf41f77d9b2b9c
Eric <ericnguyen23@gmail.com>
2023-04-17 17:49:39 -0700

    Added routes in server.js to get all users and thoughts

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const { User } = require("../models/User");
+
+// currently not working
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./config/connection");
 
 // require Model
-const { User } = require("./models");
+const { User, Thought } = require("./models");
 
 // initiate express
 const PORT = process.env.PORT || 3001;
@@ -11,6 +11,28 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// get all users
+app.get("/all-users", (req, res) => {
+  User.find({}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(500).json({ message: "something went wrong" });
+    }
+  });
+});
+
+// get all thoughts
+app.get("/all-thoughts", (req, res) => {
+  Thought.find({}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(500).json({ message: "something went wrong" });
+    }
+  });
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
